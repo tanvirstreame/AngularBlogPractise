4a309d5b7c5185a80210a39d8b9855cee34e3884
tanvirstreame <streametanvir@gmail.com>
2019-01-19 06:05:35 +0600

    init

---
+++ src/app/about/about.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-about',
+  template: './about.html',
+  styles: []
+})
+export class AboutComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
